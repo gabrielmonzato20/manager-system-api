106824a546a060b71eeacd16b29ab0c70044cc2b
gabrielmonzato <gabrielmonzato20@gmail.com>
2021-02-01 17:06:34 -0300

    feature-categorryresource

---
+++ src/main/java/com/coursedash/client/resource/CategoryResource.java
@@ -0,0 +1,55 @@
+package com.coursedash.client.resource;
+
+import com.coursedash.client.model.Category;
+import com.coursedash.client.repository.CategoryRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
+@RestController
+@RequestMapping("/categorys")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        List<Category> categorys = categoryRepository.findAll();
+        return ResponseEntity.ok(categorys);
+        //return  !categorys.isEmpty()?    ResponseEntity.ok(categorys):ResponseEntity.noContent().build();
+    }
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Category> save(@RequestBody Category category,HttpServletResponse response){
+        Category categorySave = categoryRepository.save(category);
+        URI uri = ServletUriComponentsBuilder.
+        fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(categorySave.getId())
+        .toUri();
+        response.setHeader("Location",uri.toASCIIString());
+        return     ResponseEntity.created(uri).body(categorySave);
+}
+@GetMapping("/{id}")
+ResponseEntity<Category> findByid(@PathVariable Long id){
+    Optional<Category> category = categoryRepository.findById(id);
+    return category.isPresent() ? ResponseEntity.ok(category.get()) :  ResponseEntity.notFound().build();
+}
+}
---
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+CREATE TABLE category(
+    id int  Primary key AUTO_INCREMENT ,
+    namecli varchar(80) NOT NULL
+)Engine=innoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO category (namecli ) VALUES ("Lazer");
+INSERT INTO category (namecli ) VALUES ("Alimentação");
+INSERT INTO category (namecli ) VALUES ("Farmacia");
+INSERT INTO category (namecli ) VALUES ("Mercado");
+INSERT INTO category (namecli ) VALUES ("Outros");
