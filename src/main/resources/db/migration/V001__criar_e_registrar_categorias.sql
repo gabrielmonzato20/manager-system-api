9499e185d66507aefa6560eed62cb21b40c2dcdb
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 13:08:05 -0400

    feature add ci cd

--- src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -3,8 +3,8 @@ CREATE TABLE category(
     namecli varchar(80) NOT NULL
 )Engine=innoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO category (namecli ) VALUES ("Lazer");
-INSERT INTO category (namecli ) VALUES ("Alimentação");
-INSERT INTO category (namecli ) VALUES ("Farmacia");
-INSERT INTO category (namecli ) VALUES ("Mercado");
-INSERT INTO category (namecli ) VALUES ("Outros");
+INSERT INTO category (namecli ) VALUES ('Lazer');
+INSERT INTO category (namecli ) VALUES ('Alimentação');
+INSERT INTO category (namecli ) VALUES ('Farmacia');
+INSERT INTO category (namecli ) VALUES ('Mercado');
+INSERT INTO category (namecli ) VALUES ('Outros');
